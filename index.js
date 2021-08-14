4e952e8970a0a0bc1ae724e8c332b4751c86025e
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-13 22:08:52 -0400

    commit tree 646cbcab01cd680c460be9b038440f8fcf1d5622

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const random = Math.floor(Math.random() * 2500 )
+const num1 = 8 
+const num2 = 7.75
+const multiply = num1 * num2 
+
+const num3 = 24
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(2,5,7,8,9,20)
